2f84d54e4a3763d20c9c00ebc4dc90bb24c7f74c
SatyrDiamond <colbydray@gmail.com>
2023-10-15 18:57:40 -0400

    update

--- decode.py
+++ decode.py
@@ -10,6 +10,14 @@ args = parser.parse_args()
 in_file = args.i
 datadef_file = args.d
 
+if in_file == None:
+	print('Input File Not Found')
+	exit()
+
+if datadef_file == None:
+	print('DataDef File Not Found')
+	exit()
+
 in_stream = open(in_file, 'rb')
 
 output_data, global_vars, pointers, pointset = datadef.parse(in_stream, datadef_file)
